199042f2c0ea4115a5d98dcf6405064201bb77c5
EliseMairet <elise.mairet@hotmail.com>
2024-07-21 10:50:08 +0200

    commit 21/07

--- P7-Dev-Web-livres-main/backend/controllers/user.js
+++ P7-Dev-Web-livres-main/backend/controllers/user.js
@@ -1,5 +1,6 @@
 const user = require ('../models/user.js')
 const bcrypt = require ('bcrypt')
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -16,7 +17,7 @@ exports.signup = (req, res, next) => {
   };
 
   exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    user.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
@@ -28,7 +29,11 @@ exports.signup = (req, res, next) => {
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            {userId: user._id},
+                            'RAMDOM_TIKEN_SECRET',
+                            {expiresIn: '24h'}
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
