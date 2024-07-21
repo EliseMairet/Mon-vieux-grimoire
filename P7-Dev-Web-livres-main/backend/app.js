86bb453997f3c3b042df294259922d900a812b41
EliseMairet <elise.mairet@hotmail.com>
2024-07-21 11:25:56 +0200

    commit 21/07

--- P7-Dev-Web-livres-main/backend/app.js
+++ P7-Dev-Web-livres-main/backend/app.js
@@ -1,28 +1,32 @@
-const express = require ("express");
-const bodyParser = require ('body-parser')
+const express = require ('express');
 const mongoose = require('mongoose');
-
-const stuffRoutes = require('./routes/stuff');
-
-app.use('/api/stuff', stuffRoutes);
-
-mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 const app = express();
+require('dotenv').config();
 
-app.use(express.json());
+const dataBaseUrl = process.env.DATABASE_URL;
 
-app.use((req, res, next) => {
+mongoose.connect(dataBaseUrl,
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app
+.use(express.json())
+.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
-  app.use('/api/stuff', stuffRoutes);
-  
+//Routes
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static('images'));
+
 module.exports = app;
